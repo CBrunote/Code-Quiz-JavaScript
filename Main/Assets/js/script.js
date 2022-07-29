a9cf6bafd0b6439618a8c6e680784c1a62bdc2c7
CBruno <christian.bruno1995@gmail.com>
2022-07-29 18:44:10 -0400

    added javascript with initial variables including array for quiz questions

---
+++ Main/Assets/js/script.js
@@ -0,0 +1,43 @@
+var question = document.querySelector("#question");
+var choices = document.querySelectorAll(".choice-text");
+var scoreText = document.querySelector("#score");
+
+
+var questions = [
+{
+    question: "Commonly used data types DO NOT inlcude"
+    choice1: "Strings" 
+    choice2: "Booleans"
+    choice3: "Alerts"
+    choice4: "Numbers"
+},
+{
+    question: "The condition of an If/Else statement is enclosed with ______."
+    choice1: "quotes" 
+    choice2: "curly brackets"
+    choice3: "parentheses"
+    choice4: "square brackets"
+},
+{
+    question: "Arrays in Javascript can be used to store ______."
+    choice1: "numbers and strings" 
+    choice2: "other strings"
+    choice3: "booleans"
+    choice4: "All Of The Above"
+},
+{
+    question: "String values must be enclosed within _____ when being assigned to variables."
+    choice1: "commas" 
+    choice2: "curly brackets"
+    choice3: "quotes"
+    choice4: "parentheses"
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:"
+    choice1: "Javascript" 
+    choice2: "terminal/bash"
+    choice3: "for loops"
+    choice4: "console.log"
+}
+]
+
