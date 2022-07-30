bffd4704df92fcdb539014f6633b145e5d1fde98
CBrunote <christian.bruno1995@gmail.com>
2022-07-30 11:09:35 -0400

    added logic to start game and pull questions into the quiz

--- Main/Assets/js/script.js
+++ Main/Assets/js/script.js
@@ -1,43 +1,101 @@
 var question = document.querySelector("#question");
-var choices = document.querySelectorAll(".choice-text");
+var choiceA = document.querySelector("#a");
+var choiceB = document.querySelector("#b");
+var choiceC = document.querySelector("#c");
+var choiceD = document.querySelector("#d");
+var choiceArr = [choiceA, choiceB, choiceC, choiceD]
 var scoreText = document.querySelector("#score");
+var homeContainer  = document.querySelector("#home");
+var gameContainer = document.querySelector("#game");
+var timerEl = document.querySelector("#timerEl");
+var progressText = document.querySelector("#progressText");
+var userAnswer = document.querySelector(".choice-text")
 
 
+var questionIndex = 0
 var questions = [
 {
-    question: "Commonly used data types DO NOT inlcude"
-    choice1: "Strings" 
-    choice2: "Booleans"
-    choice3: "Alerts"
-    choice4: "Numbers"
+    question: "Commonly used data types DO NOT inlcude",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
 },
 {
-    question: "The condition of an If/Else statement is enclosed with ______."
-    choice1: "quotes" 
-    choice2: "curly brackets"
-    choice3: "parentheses"
-    choice4: "square brackets"
+    question: "The condition of an If/Else statement is enclosed with ______.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
 },
 {
-    question: "Arrays in Javascript can be used to store ______."
-    choice1: "numbers and strings" 
-    choice2: "other strings"
-    choice3: "booleans"
-    choice4: "All Of The Above"
+    question: "Arrays in Javascript can be used to store ______.",
+    choices: ["numbers and strings", "other strings", "booleans", "all of the above"],
+    answer: "all of the above"
 },
 {
-    question: "String values must be enclosed within _____ when being assigned to variables."
-    choice1: "commas" 
-    choice2: "curly brackets"
-    choice3: "quotes"
-    choice4: "parentheses"
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
 },
 {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:"
-    choice1: "Javascript" 
-    choice2: "terminal/bash"
-    choice3: "for loops"
-    choice4: "console.log"
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "terminal/bash", "for loops","console.log"],
+    answer: "console.log"
 }
 ]
 
+// function to display question and choices on game and change to next question when an answer is chosen
+function initialize() {
+    question.textContent = questions[questionIndex].question;
+    for (var i = 0; i < choiceArr.length; i++) {
+        choiceArr[i].textContent = questions[questionIndex].choices[i];
+        choiceArr[i].onclick = function() { 
+            console.log(questions[questionIndex].choices[i] === questions.answer);
+            // if (questions[questionIndex].choices[i] == questions.answer) {
+            //     choiceArr[i].dataset.isAnswer = true
+            // } else {
+            //     choiceArr[i].dataset.isAnswer = false
+            // }
+            console.log(this.textContent);
+            // console.log(choiceArr[i].dataset.isAnswer);
+            questionIndex ++;
+            initialize();
+            progressText.textContent = (questionIndex + 1) + " of " + questions.length;
+        };
+    }
+}
+
+//comparison function for correct or incorrect answer
+function comparison() {
+    console.log(userAnswer.getAttribute("data-answer"));
+    if (userAnswer.getAttribute("data-answer") === questions.answer){
+        userAnswer.classList.add("correct");
+    }else {
+        userAnswer.classList.add("incorrect");
+    }
+    };
+
+
+// Function to set timer at 60 seconds and count down each second
+function setTime() {
+    var secondsLeft = 60;
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timerEl.textContent = "0:" + secondsLeft;
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        ;
+      }if(secondsLeft < 10) {
+        timerEl.textContent = "0:0" + secondsLeft;
+      }
+    }, 1000);
+  }
+
+// click event to hide home screen and display quiz
+document.querySelector(".btn").onclick = function () {
+    homeContainer.classList.add("hidden");
+    gameContainer.classList.remove("hidden");
+    initialize();
+    setTime();
+    progressText.textContent = (questionIndex + 1) + " of " + questions.length;
+}
